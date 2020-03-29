2af1e4bb46524b5a163e848c21c705c8037b5860
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-03-28 17:17:30 -0700

    Create project structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import axios from 'axios';
+
+export default class PostsViewer extends Component {
+
+  render() {
+    return (
+      <Text>Posts Viewer</Text>
+    );
+  }
+}
